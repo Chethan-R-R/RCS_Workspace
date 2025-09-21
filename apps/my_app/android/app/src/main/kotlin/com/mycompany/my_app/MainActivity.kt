908a7488251fdf4bdbd6670b25a52e08a2d1d821
Chethan R R <chethan@9980rr@gmail.com>
2025-09-21 10:55:30 +0530

    initial commit

--- apps/my_app/android/app/src/main/kotlin/com/mycompany/my_app/MainActivity.kt
+++ apps/my_app/android/app/src/main/kotlin/com/mycompany/my_app/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import android.os.SystemClock
 
 class MainActivity : FlutterActivity() {
     private val flutterNativeChannel = "RCS_App/nativeChannel"
